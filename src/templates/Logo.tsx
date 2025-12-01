368c57eaf080f5f5687f12aff86379042c53bdf3
SathokZapatrin <132094434+SathokZapatrin@users.noreply.github.com>
2025-12-01 22:36:22 +0200

    Update Logo.tsx

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -7,7 +7,7 @@ type ILogoProps = {
 const Logo = (_props: ILogoProps) => {
   return (
     <span className="inline-flex items-center">
-      <Image src="/I_arey_uda.png" alt="Logo" width={64} height={64} />
+      <Image src="/I_arey_uda.png" alt="Logo" width={150} height={150} />
     </span>
   );
 };
